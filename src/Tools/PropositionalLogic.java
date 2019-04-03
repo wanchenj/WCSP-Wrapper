ad784bb47492421e783ac0cc82eaf460147ec1b2
Wanchen Jiang <wanchenj@usc.edu>
2019-04-03 00:30:03 -0700

    Logic Part Done

--- src/Tools/PropositionalLogic.java
+++ src/Tools/PropositionalLogic.java
@@ -1,30 +1,84 @@
-// put this in the Tools package
 package Tools;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.Stack;
-public class PropositionalLogic 
-{
-    // constructor
-    public PropositionalLogic()
-    {
-    	// do nothing i guess
-    }
 
-    private static String postfix(String infix)
+import static java.lang.Math.log;
+
+public class PropositionalLogic
+{
+	private String infix;
+	private String postfix;
+	private int variableAmount;
+	private int variableMax;
+	private ArrayList <Boolean> truthTable;
+	
+	 public static void main(String[] args)
+	    {
+		 
+		 double possibility = 0.8;
+	     // usable tokens
+		 System.out.println("Tokens:\n>: implication\n=: biconditional\n&: conjunction\nv: disjunction\n@: exclusive disjunction\n~: negation\n");
+
+	     //get a logical expression from the user
+	     System.out.println("Enter a logical expression:");
+	        
+		 Scanner scan = new Scanner(System.in);
+		 String inputPropositionalLogic = scan.next();
+		 PropositionalLogic logicSolver = new PropositionalLogic(inputPropositionalLogic, 3);
+		 List<Boolean> truthTable = logicSolver.GenerateTruthTable();
+		 List<Double> truthTableWeights = logicSolver.truthTableToWeight(truthTable, possibility);
+	    }
+	 
+	public PropositionalLogic(String inputPropositionalLogic, int variableAmount){
+
+        infix = inputPropositionalLogic;
+        this.variableAmount = variableAmount;
+        this.variableMax = variableAmount - 1;
+        //infix = "(P&Q)>(PvQ)"; //tautology example
+        //infix = "(~PvQ)=(P>Q)"; //tautology example
+        //infix = "P>(P>(P>P))"; //tautology example
+
+        //infix = "(P&~P)"; //contradiction example
+        //infix = "(P>R)&(Q>R)"; //contingency example
+       
+    }
+	
+	public ArrayList<Boolean> GenerateTruthTable() {
+		
+			truthTable = new ArrayList <Boolean>();
+			
+		 	System.out.println("Infix expression: " + infix);
+	        postfix = postfix(infix); //convert expression to postfix
+	        System.out.println("Postfix expression: " + postfix); //display postfix expression
+
+	      //print a truth table
+	        for(int i = 0; i < variableAmount; i++) {
+	        		System.out.print(i + "\t");
+	        }
+//	        System.out.println("0\t1\t2\t"+); 
+	        System.out.println(infix);
+	        System.out.println("-------------------------------------");
+	        return evaluate(postfix);
+
+	}
+
+    public String postfix(String infix)
     {
         Stack<Character> stack = new Stack<>(); //operator stack
         stack.push('#'); //initialize stack with # token
-        
+
         String postfix = "";
         char c; //current opperand/opperator being processed
-        
-        
+
+
         // parse Infix expression into stack
         for(int i=0; i<infix.length(); i++)
         {
             c = Character.toUpperCase(infix.charAt(i)); //process the infix expression, one opperand/opperator at a time
-            if(c=='P'||c=='Q'||c=='R') //add propositions to the postfix
+            if(c>='0' && c <= ('0' + variableMax)) //add propositions to the postfix
             {
                 postfix += c;
             }
@@ -59,8 +113,8 @@ public class PropositionalLogic
             }
         return postfix;
     }
-    
-    public static int priority(int token)
+
+    public int priority(int token)
     {
         if(token=='=') //biconditional
             return 1;
@@ -76,86 +130,38 @@ public class PropositionalLogic
             return 6;
         if(token=='(') //open bracket
             return 0;
-            
-        return 0; 
+
+        return 0;
     }
-    
-    public static String evaluate(String postfix)
+
+    public ArrayList <Boolean> evaluate(String postfix)
     {
         Stack<Character> stack = new Stack<>(); //value stack
         stack.push('#'); //initialize stack with # token
-        
+
         char c; // current opperand/opperator being processed
         char t='T',f='F'; // true and false
         int p,q; //propositional variables
         int tCount=0, fCount=0; //counters for how many times True and False appear
-
-        for(int set=0; set<8; set++)
+        
+//        System.out.println(variableAmount);
+        
+        for(int set=0; set< (Math.pow(2, variableAmount)); set++)
         {
+//        		System.out.println("In loop for 2^3");
             for(int i=0; i<postfix.length(); i++)
             {
                 c = postfix.charAt(i); //process the postfix expression
-                if(set==0 && (c=='P' || c=='Q' || c=='R'))
-                {
-                    stack.push(t);
-                }
-                else if(set==1 && (c=='P' || c=='Q' || c=='R'))
-                {
-                    if(c=='P')
-                        stack.push(t);
-                    if(c=='Q')
-                        stack.push(f);
-                    if(c=='R')
-                        stack.push(t);
-                }
-                else if(set==2 && (c=='P' || c=='Q' || c=='R'))
-                {
-                    if(c=='P')
-                        stack.push(f);
-                    if(c=='Q')
-                        stack.push(t);
-                    if(c=='R')
-                        stack.push(t);
+                if(c>='0' && c<=('0' + variableMax)){
+//                    boolean[] a = new boolean[10];
+                    char temp;
+                    int s=set;
+                    for(int j = variableMax; j >= 0; j--){
+                        temp= s % 2==1 ? t:f;
+                        s/=2;
+                        if(c==j+'0') stack.push(temp);
                     }
-                else if(set==3 && (c=='P' || c=='Q' || c=='R'))
-                {
-                    if(c=='P')
-                        stack.push(f);
-                    if(c=='Q')
-                        stack.push(f);
-                    if(c=='R')
-                        stack.push(t);
-                }
-                else if(set==4 && (c=='P' || c=='Q' || c=='R'))
-                {
-                    if(c=='P')
-                        stack.push(t);
-                    if(c=='Q')
-                        stack.push(t);
-                    if(c=='R')
-                        stack.push(f);
-                }
-                else if(set==5 && (c=='P' || c=='Q' || c=='R'))
-                {
-                    if(c=='P')
-                        stack.push(t);
-                    if(c=='Q')
-                        stack.push(f);
-                    if(c=='R')
-                        stack.push(f);
-                }
-                else if(set==6 && (c=='P' || c=='Q' || c=='R'))
-                {
-                    if(c=='P')
-                        stack.push(f);
-                    if(c=='Q')
-                        stack.push(t);
-                    if(c=='R')
-                        stack.push(f);
-                }
-                else if(set==7 && (c=='P' || c=='Q' || c=='R'))
-                {
-                    stack.push(f);
+
                 }
                 else if(c=='>') //evaluate an implication
                 {
@@ -169,7 +175,7 @@ public class PropositionalLogic
                         stack.push(t); //F -> T = T
                     else if(p=='F' && q=='F')
                         stack.push(t); //F -> F = T
-                }   
+                }
                 else if(c=='~') //evaluate a negation
                 {
                     p = stack.pop();
@@ -232,35 +238,69 @@ public class PropositionalLogic
                 }
             }
             c = (char)stack.pop(); //pop truth value of the current case
-            if(set==0)
-                System.out.print("T\tT\tT\t");
-            if(set==1)
-                System.out.print("T\tF\tT\t");
-            if(set==2)
-                System.out.print("F\tT\tT\t");
-            if(set==3)
-                System.out.print("F\tF\tT\t");
-            if(set==4)
-                System.out.print("T\tT\tF\t");
-            if(set==5)
-                System.out.print("T\tF\tF\t");
-            if(set==6)
-                System.out.print("F\tT\tF\t");
-            if(set==7)
-                System.out.print("F\tF\tF\t");
-                
+
+            int s = set;
+            
+            boolean[] lineOfTruthTable = new boolean[variableAmount];
+            for(int j = variableMax; j >= 0; j--){
+                if(s % 2 == 1){
+                		lineOfTruthTable[j]=true;
+                }
+                else lineOfTruthTable[j]=false;
+                s /= 2;
+            }
+            
+            for(boolean b:lineOfTruthTable){
+                System.out.print(b+"\t");
+            }
+
             System.out.println(c); //print truth value for user entered expression
-            if(c=='T') 
-                tCount++; //count truths
-            else
-                fCount++; //count falses
+            if(c=='T') {
+            		truthTable.add(true);
+            		tCount++; //count truths
+            }   
+            else {
+            		truthTable.add(false);
+            		fCount++; //count falses
+            }
         }
+        String logicType = "";
         //Determine the type of the logical expression
-        if(tCount==8)//all cases are true
-            return "tautology.";
-        else if(fCount==8) //all cases are false
-            return "contradiction.";
+        if(tCount == Math.pow(2, variableAmount))//all cases are true
+        		logicType = "tautology.";
+        else if(fCount == Math.pow(2, variableAmount)) //all cases are false
+        		logicType = "contradiction.";
         else //mixed case
-            return "contingency.";
-    } 
+        		logicType = "contingency.";
+        System.out.println(infix+" is a "+ logicType); // evaluate the postfix expression
+        System.out.println();
+        return truthTable;
+    }
+
+    public List<Double> truthTableToWeight(List<Boolean> truthTable, double possibility){
+        int tNum=0;
+        for (Boolean entry : truthTable) {
+            if (entry) {
+                tNum++;
+            }
+        }
+        Double truthWeight =  -log( possibility / (double) tNum);
+        Double falseWeight =  -log( (1 - possibility) / (double) (truthTable.size() - tNum));
+
+        List<Double> weights = new ArrayList<>();
+        for (Boolean entry : truthTable) {
+            if (entry) {
+                weights.add(truthWeight);
+            }
+            else{
+                weights.add(falseWeight);
+            }
+        }
+        for (double weight: weights) {
+        		System.out.println(weight);
+        }
+        return weights;
+    }
+
+
 }

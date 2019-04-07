906168c7902f91e3892ce93d68de5d38d84f6771
usmansohail <msohail@usc.edu>
2019-04-06 17:45:54 -0700

    display truth value of variable title

--- src/Tools/read_results.py
+++ src/Tools/read_results.py
@@ -1,3 +1,8 @@
+import re
+import sys
+
+print(sys.argv[1])
+
 # open the file
 file = open('wcsp-output.txt', 'r')
 
@@ -5,12 +10,36 @@ file = open('wcsp-output.txt', 'r')
 assignments = {}
 inResults = False
 
+def find_var(var, value):
+    # open the original swc file:
+    swc = open(sys.argv[1], 'r')
+
+    # read the list and find the variable with the same name
+    for line in swc.readlines():
+        line = line.strip()
+        if line != '':
+            if line[0] == 'v':
+                # this means this line is a variable, now check if the next character is var
+                if line[1] == var:
+                    # get the name in the parenthesis
+                    regex = re.compile(r'\(.*\)')
+                    #find the regex
+                    result = re.findall(regex, line)
+                    # take out the parantheses
+                    result =  result[0]
+                    status = ' false '
+                    if value == 1:
+                        status = " true "
+                    print('The variable: ', result, ' is ', status)
+
 for line in file.readlines():
-    if ("Best assignments:" in line) or inResults:
-      inResults = True
-      line_elements = line.split(' ')
-      print(line)
-      if len(line_elements) == 2:
-        assignments[line_elements[0]] = line_elements[1]
-        print(line)
+    line_elements = line.split(' ')
+    if ("Solution" in line):
+        print('\n', line)
+    elif len(line_elements) == 2:
+        if line[0] == 'Weight':
+            print(line)
+        else:
+            find_var(line_elements[0], line_elements[1])
+
 
